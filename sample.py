b0cdfce4d512ec307561bd225935d064b68a3507
Ryosuke Tajima <ryosuke.tajima@gmail.com>
2015-07-29 14:42:26 +0000

    change initial text as unicode

--- sample.py
+++ sample.py
@@ -50,7 +50,7 @@ prev_char = np.array([0])
 if args.gpu >= 0:
     prev_char = cuda.to_gpu(prev_char)
 if len(args.primetext) > 0:
-    for i in args.primetext:
+    for i in unicode(args.primetext, 'utf-8'):
         sys.stdout.write(i)
         prev_char = np.ones((1,)).astype(np.int32) * vocab[i]
         if args.gpu >= 0:
