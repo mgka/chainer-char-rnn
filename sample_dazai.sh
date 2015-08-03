6abcbb19799b9519a37a5414b3b997cdbefa3b88
Ryosuke <ryosuke.tajima@gmail.com>
2015-08-03 09:57:14 +0900

    after 2-days training.

--- sample_dazai.sh
+++ sample_dazai.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-python sample.py --model $1 --vocabulary data/dazai/vocab.bin --primetext 'メロスは' --length 128
+python sample.py --model $1 --vocabulary data/dazai/vocab.bin --primetext 'メロスは' --length 2000
 
 # python sample.py --model $1 --vocabulary data/dazai/vocab.bin --length 128
--- train_dazai.sh
+++ train_dazai.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-python train.py --data_dir data/dazai --checkpoint_dir cv/dazai --init_from "$1"
+python train.py \
+    --data_dir data/dazai \
+    --checkpoint_dir /media/tajima/New\ Volume/cv/dazai \
+    --rnn_size 1024
+    --init_from "$1"
+
